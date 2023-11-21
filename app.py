fb090b49b54db2dd48a7bce67480decc3b3c241b
Whimss <sanchez.alondra7@gmail.com>
2023-11-20 16:58:29 -0800

    adding mongo db
    added a mongodb to test out if it worked for now

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from pymongo.mongo_client import MongoClient
+from flask import Flask, render_template, url_for,request
 
 app = Flask(__name__)
 
@@ -14,6 +15,18 @@ def home():
 def about():
     return render_template('about.html')
 
+client = MongoClient('localhost', 27017)
+db = client["cwp_prod"]
+coll = db.main
+
+@app.route('/dbtest')
+def db_test():
+
+    year_param = request.args.get('year')
+
+    # 1
+    _1 = coll.find({"year": int(year_param)}, {"_id": 0})
+    return(list(_1))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
